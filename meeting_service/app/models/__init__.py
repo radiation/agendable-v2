57f424ea1430bc0aba1da2b9e920af0f69aefc84
Bryan Choate <bryan.choate@gmail.com>
2024-07-20 15:07:22 -0400

    Model package definition is being overwritten for some reason

---
+++ meeting_service/app/models/__init__.py
@@ -0,0 +1,6 @@
+from .base import Base
+from .meeting import Meeting
+from .meeting_attendee import MeetingAttendee
+from .meeting_recurrence import MeetingRecurrence
+from .meeting_task import MeetingTask
+from .task import Task
