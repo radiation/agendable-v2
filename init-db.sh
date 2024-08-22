ac13da78b88750744c2c8adf83f1ff87c64b5f4f
Bryan Choate <bryan.choate@gmail.com>
2024-08-22 18:08:04 -0400

    Initial kong structure

--- init-db.sh
+++ init-db.sh
@@ -5,3 +5,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
   SELECT 'CREATE DATABASE meeting_db'
   WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'meeting_db')\gexec
 EOSQL
+echo "Creating Kong database..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+    CREATE DATABASE kong_db;
+    GRANT ALL PRIVILEGES ON DATABASE kong_db TO user;
+EOSQL
+echo "Database created."
