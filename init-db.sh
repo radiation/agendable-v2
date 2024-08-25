240063d39d0b0abeb6d3d937216dfca2ff3bc937
Bryan Choate <bryan.choate@gmail.com>
2024-08-25 11:10:47 -0400

    Use a script for routes instead of kongfig.

--- init-db.sh
+++ init-db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+echo "Creating Meeting database..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
   SELECT 'CREATE DATABASE meeting_db'
   WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'meeting_db')\gexec
