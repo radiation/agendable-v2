9850894da7f6f731901274d96af9467443d37539
Bryan Choate <bryan.choate@gmail.com>
2024-08-25 21:52:28 -0400

    Incremental kong progress

--- kong/scripts/configure.sh
+++ kong/scripts/configure.sh
@@ -10,12 +10,19 @@ echo "Adding services & routes to ${KONG_URL}..."
 curl -i -X POST \
   --url ${KONG_URL}/services/ \
   --data 'name=user_service' \
-  --data 'url=http://user_service:8005'
+  --data 'url=http://user_service:8004'
 
-# Routes
+# Docs
 curl -i -X POST \
   --url ${KONG_URL}/services/user_service/routes \
-  --data 'name=users_route' \
+  --data 'name=user_service_docs' \
+  --data 'paths[]=/user-service/docs' \
+  --data 'strip_path=false'
+
+# Other Routes
+curl -i -X POST \
+  --url ${KONG_URL}/services/user_service/routes \
+  --data 'name=auth_route' \
   --data 'paths[]=/auth' \
   --data 'strip_path=false'
 
@@ -33,35 +40,49 @@ curl -i -X POST \
 curl -i -X POST \
   --url ${KONG_URL}/services/ \
   --data 'name=meeting_service' \
-  --data 'url=http://meeting_service:8005'
+  --data 'url=http://meeting_service:8005' \
+  --data "name=jwt"
 
-# Routes
+# Docs
+curl -i -X POST \
+  --url ${KONG_URL}/services/meeting_service/routes \
+  --data 'name=meeting_service_docs' \
+  --data 'paths[]=/meeting-service/docs' \
+  --data 'strip_path=false' \
+  --data "name=jwt"
+
+# Other Routes
 curl -i -X POST \
   --url ${KONG_URL}/services/meeting_service/routes \
   --data 'name=meetings_route' \
   --data 'paths[]=/meetings' \
-  --data 'strip_path=false'
+  --data 'strip_path=false' \
+  --data "name=jwt"
 
 curl -i -X POST \
   --url ${KONG_URL}/services/meeting_service/routes \
   --data 'name=recurrences_route' \
   --data 'paths[]=/meeting_recurrences' \
-  --data 'strip_path=false'
+  --data 'strip_path=false' \
+  --data "name=jwt"
 
 curl -i -X POST \
   --url ${KONG_URL}/services/meeting_service/routes \
   --data 'name=attendees_route' \
   --data 'paths[]=/meeting_attendees' \
-  --data 'strip_path=false'
+  --data 'strip_path=false' \
+  --data "name=jwt"
 
 curl -i -X POST \
   --url ${KONG_URL}/services/meeting_service/routes \
   --data 'name=tasks_route' \
   --data 'paths[]=/tasks' \
-  --data 'strip_path=false'
+  --data 'strip_path=false' \
+  --data "name=jwt"
 
 curl -i -X POST \
   --url ${KONG_URL}/services/meeting_service/routes \
   --data 'name=meeting_tasks_route' \
   --data 'paths[]=/meeting_tasks' \
-  --data 'strip_path=false'
+  --data 'strip_path=false' \
+  --data "name=jwt"
